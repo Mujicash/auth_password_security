2c40d6832030e0c9ab96832234e2d29ed627a664
Andre007 <andre27012001@gmail.com>
2025-05-02 12:34:09 -0500

    [add] post_init_hook

---
+++ hooks.py
@@ -0,0 +1,12 @@
+def post_init_hook(cr, registry):
+    # Set password date for already existing users
+    cr.execute(
+        """
+        UPDATE
+            res_users
+        SET
+            password_write_date = NOW() at time zone 'UTC'
+        WHERE
+            password_write_date IS NULL;
+    """
+    )
